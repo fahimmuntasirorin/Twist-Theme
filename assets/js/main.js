78fa3ad800a157d3df9771cd178655aabf41b647
developerorin2002 <developerorin2002@gmail.com>
2023-10-21 14:10:35 +0600

    client section done

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    // counter up 
+    $('.project-counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+    // magnific popup 
+    $('.project-img').magnificPopup({
+        type: 'image',
+
+        gallery: {
+            enabled: true
+        }
+
+    });
+    // testimonial slider
+    $('.testimonial-slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay:true,
+        autoplaySpeed: 1000,
+    });
+    // client slider
+    $('.clients').slick({
+        arrows: false,
+        dots: false,
+        autoplay:true,
+        autoplaySpeed: 1000,
+        slidesToShow:4
+    });
+    // orinhossain
+});
